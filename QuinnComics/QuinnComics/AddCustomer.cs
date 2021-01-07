b0a3b9262bbbdf92e8b51d7263e53499f7bac8a3
deleonricardo <70586693+deleonricardo@users.noreply.github.com>
2021-01-07 10:40:06 -0600

    Add files via upload

---
+++ QuinnComics/QuinnComics/AddCustomer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuinnComics
+{
+    public partial class AddCustomer : Form
+    {
+        public AddCustomer()
+        {
+            InitializeComponent();
+        }
+
+   private void AddCustomer_Load(object sender, EventArgs e)
+        {
+            //clear all fields
+           
+            txtFN.Clear();
+            txtLN.Clear();
+            txtPhone.Clear();
+            txtEmail.Clear();
+        }
+
+ private void btnCancel_Click(object sender, EventArgs e)
+        {
+            //clear all fields
+            
+            txtFN.Clear();
+            txtLN.Clear();
+            txtPhone.Clear();
+            txtEmail.Clear();
+            this.Hide();
+        }
+
+        private void btnAddCustomer_Click(object sender, EventArgs e)
+        {
+            //variables, db connection & command
+            string sConnection = null;
+            OleDbConnection dbConnection;
+            OleDbCommand dbCommand;
+            string sql = null;
+
+            try
+            {
+                sConnection = ("Provider=Microsoft.ACE.OLEDB.12.0;" + "Data Source=ComicStore.accdb"); //connection to database
+                //sql statement for insert
+                sql = "INSERT INTO CustomerTable (FirstName, LastName, PhoneNumber, Email) VALUES ('" + txtFN.Text + "', '" + txtLN.Text + "', '" + txtPhone.Text + "', '" + txtEmail + "');";
+                dbConnection = new OleDbConnection(sConnection);
+
+                dbConnection.Open();
+                dbCommand = new OleDbCommand(sql, dbConnection);
+                dbCommand.ExecuteNonQuery();
+                dbCommand.Dispose();
+                dbConnection.Close();
+
+            }
+            catch(Exception a)
+            {
+                MessageBox.Show("Error Opening " + a.ToString());
+            }
+            this.Hide();
+        }
+
+       
+    }
+}
---
+++ QuinnComics/QuinnComics/CustomerList.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuinnComics
+{
+    public partial class CustomerList : Form
+    {
+        public CustomerList()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddBook_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                comics newBook = new comics();  //create new comic - book object
+                newBook.book = txtBook.Text;
+                Form1.cart.Add(newBook); //add book to cart
+
+                RefreshBookList();
+
+                //empty textbox
+                txtBook.Clear();
+            }
+            catch(Exception b)
+            {
+                MessageBox.Show(b.Message);
+            }
+        }
+
+        private void RefreshBookList()
+        {
+            try
+            {
+                listCBooks.Items.Clear();
+                foreach (comics c in Form1.cart)
+                {
+                    listCBooks.Items.Add(c);
+                }
+            }
+            catch(Exception r)
+            {
+                MessageBox.Show(r.Message);
+            }
+
+        }
+
+        private void btnCancelList_Click(object sender, EventArgs e)
+        {
+            Form1.cart.Clear(); //clear cart to cancel
+            this.Hide();
+
+        }
+
+        private void btnDeleteBook_Click(object sender, EventArgs e)
+        {
+            listCBooks.Items.Remove(listCBooks.Items[listCBooks.SelectedIndex]);
+        }
+    }
+}
